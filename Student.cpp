2c3842e553b6fe94e4793fef1f5da4793e09a735
khushiPatelA <104723833+khushiPatelA@users.noreply.github.com>
2022-09-21 13:58:39 +0930

    working student cpp

--- Student.cpp
+++ Student.cpp
@@ -1,34 +1,29 @@
+#include "Student.h"
+
 #include <iostream>
 #include <string>
-#include "PersonalDetails.h"
+
 #include "Person.h"
-#include "Student.h"
+#include "PersonalDetails.h"
 using namespace std;
 
-Student::Student(){
-    name = "Null";
-    age = 0;
-    email= "Null";
-    dateBD = 0;
-    monthBD = 0;
-    yearBD = 0;
-    ID = 0;
-    gender = "Null"; 
+Student::Student() {
+  name = "Null";
+  age = 0;
+  email = "Null";
+  dateBD = 0;
+  monthBD = 0;
+  yearBD = 0;
+  ID = 0;
+  gender = "Null";
+  role = "Student"; 
+}
+void Student::setRole() { role = "Student"; }
+string Student::getRole() { return role; }
+
+int Student::yearlevel(int level) {
+  year_level = level;
+  return year_level;
 }
 
-string Student::setRoll(){
-    role = "Student";
-}
-string Student::getRoll(){
-    return role;
-}
-
-int Student::yearlevel(int level){
-    year_level = level;
-    return year_level;
-}
-
-Student::~Student(){
-
-}
-
+Student::~Student() {}
